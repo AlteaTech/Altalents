096088b67db4fda0ed5e88fb2cf86c8184e7639e
AlteaTech <tech@altea-si.com>
2024-06-26 09:45:41 +0200

    fix recherche (#5)
    Co-authored-by: Jérémie <jeremie.vidal13@gmail.com>

---
+++ Commun/Altalents.DataAccess/Migrations/20240626072241_StatutUtilisateurEnBdd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altalents.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class StatutUtilisateurEnBdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Statut",
+                table: "Utilisateurs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Utilisateurs",
+                keyColumn: "Id",
+                keyValue: new Guid("f05d0f23-00b2-47fe-91ab-59ebeaa867ee"),
+                column: "Statut",
+                value: "Admin");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Statut",
+                table: "Utilisateurs");
+        }
+    }
+}
