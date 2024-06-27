f942d68534aad5658766e51bcfc759b051d89c3d
Alicia Lan <alan@altea-si.com>
2024-06-27 17:50:46 +0200

    routing

--- ui/Altalents.MVC/Angular/Altalents-client/src/app/shared/constantes/constantes-routes.ts
+++ ui/Altalents.MVC/Angular/Altalents-client/src/app/shared/constantes/constantes-routes.ts
@@ -1,4 +1,13 @@
 export class ConstantesRoutes {
+    // Bases url
     public static accueilBaseUrl: string = "accueil/";
+    public static donneesLegalesBaseUrl: string = "donnees-legales/";
+    public static formationsBaseUrl: string = "formations/";
+    public static experiencesBaseUrl: string = "experiences/";
+    public static competencesBaseUrl: string = "competences/";
+    public static questionsBaseUrl: string = "questions/";
+    public static recapitulatifBaseUrl: string = "recapitulatif/";
+    
+    // Paramètres
     public static paramTokenDossierTechnique: string = "tokenDossierTechnique";
 }
