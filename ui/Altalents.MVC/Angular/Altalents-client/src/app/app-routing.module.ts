a1f2f01ce1c288deb2c4d837768329436a35bc04
Alicia Lan <alan@altea-si.com>
2024-06-28 11:11:07 +0200

    Revert "routing"
    This reverts commit f942d68534aad5658766e51bcfc759b051d89c3d.

--- ui/Altalents.MVC/Angular/Altalents-client/src/app/app-routing.module.ts
+++ ui/Altalents.MVC/Angular/Altalents-client/src/app/app-routing.module.ts
@@ -2,21 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { ConstantesRoutes } from './shared/constantes/constantes-routes';
-import { DonneesLegalesComponent } from './components/donnees-legales/donnees-legales.component';
-import { FormationsComponent } from './components/formations/formations.component';
-import { ExperiencesComponent } from './components/experiences/experiences.component';
-import { CompetencesComponent } from './components/competences/competences.component';
-import { QuestionsComponent } from './components/questions/questions.component';
-import { RecapitulatifComponent } from './components/recapitulatif/recapitulatif.component';
 
 const routes: Routes = [
   { path: `${ConstantesRoutes.accueilBaseUrl}:${ConstantesRoutes.paramTokenDossierTechnique}`, component: AccueilComponent },
-  { path: `${ConstantesRoutes.donneesLegalesBaseUrl}:${ConstantesRoutes.paramTokenDossierTechnique}`, component: DonneesLegalesComponent },
-  { path: `${ConstantesRoutes.formationsBaseUrl}:${ConstantesRoutes.paramTokenDossierTechnique}`, component: FormationsComponent },
-  { path: `${ConstantesRoutes.experiencesBaseUrl}:${ConstantesRoutes.paramTokenDossierTechnique}`, component: ExperiencesComponent },
-  { path: `${ConstantesRoutes.competencesBaseUrl}:${ConstantesRoutes.paramTokenDossierTechnique}`, component: CompetencesComponent },
-  { path: `${ConstantesRoutes.questionsBaseUrl}:${ConstantesRoutes.paramTokenDossierTechnique}`, component: QuestionsComponent },
-  { path: `${ConstantesRoutes.recapitulatifBaseUrl}:${ConstantesRoutes.paramTokenDossierTechnique}`, component: RecapitulatifComponent },
 ];
 
 @NgModule({
--- ui/Altalents.MVC/Angular/Altalents-client/src/app/shared/constantes/constantes-routes.ts
+++ ui/Altalents.MVC/Angular/Altalents-client/src/app/shared/constantes/constantes-routes.ts
@@ -1,13 +1,4 @@
 export class ConstantesRoutes {
-    // Bases url
     public static accueilBaseUrl: string = "accueil/";
-    public static donneesLegalesBaseUrl: string = "donnees-legales/";
-    public static formationsBaseUrl: string = "formations/";
-    public static experiencesBaseUrl: string = "experiences/";
-    public static competencesBaseUrl: string = "competences/";
-    public static questionsBaseUrl: string = "questions/";
-    public static recapitulatifBaseUrl: string = "recapitulatif/";
-    
-    // Paramètres
     public static paramTokenDossierTechnique: string = "tokenDossierTechnique";
 }
