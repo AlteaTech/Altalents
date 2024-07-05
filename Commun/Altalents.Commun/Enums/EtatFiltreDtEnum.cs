74aadd1df0feb16a4f0f6e3816ca32d49c81731a
AlteaTech <tech@altea-si.com>
2024-07-05 10:57:52 +0200

    dashboard (#9)
    * preparation dts
    
    * retour mr
    
    ---------
    
    Co-authored-by: Jeremie Vidal <jeremie.vidal13@gmail.com>

---
+++ Commun/Altalents.Commun/Enums/EtatFiltreDtEnum.cs
@@ -0,0 +1,8 @@
+namespace Altalents.Commun.Enums
+{
+    public enum EtatFiltreDtEnum
+    {
+        InProgress = 0,
+        AController = 1
+    }
+}
--- Commun/Altalents.IBusiness/IServices/IDossierTechniqueService.cs
+++ Commun/Altalents.IBusiness/IServices/IDossierTechniqueService.cs
@@ -1,7 +1,10 @@
+using Altalents.Commun.Enums;
+
 namespace Altalents.IBusiness.IServices
 {
     public interface IDossierTechniqueService : IInjectableService
     {
         IQueryable<DossierTechniqueDto> GetBibliothequeDossierTechniques();
+        IQueryable<DossierTechniqueEnCoursDto> GetDtsEnCours(EtatFiltreDtEnum etat);
     }
 }
