d5cb9baa6416a7c04efe26d5138129554dda8996
Jérémie <jeremie.vidal13@gmail.com>
2024-07-16 10:23:42 +0200

    maj ui

--- ui/Altalents.MVC/Constantes/RedirectionConstantes.cs
+++ ui/Altalents.MVC/Constantes/RedirectionConstantes.cs
@@ -3,7 +3,7 @@ namespace Altalents.MVC.Constantes
     public static class RedirectionConstantes
     {
         public static string Home => "~/";
-        public static string LoggedHome => "~/Admin/HomeAdmin";
+        public static string LoggedHome => "~/Admin/TableauDeBord";
         public static string LoginPage => "~/Admin/Account/Login";
         public static string ReLogin => "/Admin/AutoLogout";
         public static string ForceLogout => "~/Admin/AutoLogout/Redirect";
