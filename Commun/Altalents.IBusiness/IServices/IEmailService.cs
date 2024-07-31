faf6071ac241d6c1ef20945b914d68d7210b46ed
Jérémie <jeremie.vidal13@gmail.com>
2024-07-31 14:48:23 +0200

    smtp

---
+++ Commun/Altalents.IBusiness/IServices/IEmailService.cs
@@ -0,0 +1,10 @@
+namespace Altalents.IBusiness.IServices
+{
+    public interface IEmailService : IInjectableService
+    {
+        void SendEmail(string toEmail, string subject, string message);
+
+        Task SendEmailAsync(string toEmail, string subject, string message, CancellationToken token = default);
+        Task SendEmailWithRetryAsync(string toEmail, string subject, string message);
+    }
+}
