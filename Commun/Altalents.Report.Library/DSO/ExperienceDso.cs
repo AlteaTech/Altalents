5f5b8fd9a01c81e4552d8b38d3ce9c67467158cf
AlteaTech <tech@altea-si.com>
2024-08-17 14:06:40 +0200

    prepare report suite (#26)
    * wip
    
    * test
    
    ---------
    
    Co-authored-by: Jérémie <jeremie.vidal13@gmail.com>

---
+++ Commun/Altalents.Report.Library/DSO/ExperienceDso.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Altalents.Report.Library.DSO
+{
+    public class ExperienceDso
+    {
+        public string IntitulePoste { get; set; }
+        public DateTime DateDebut { get; set; }
+        public DateTime? DateFin { get; set; }
+    }
+}
---
+++ Commun/Altalents.Report.Library/DossierCompetance.cs
@@ -0,0 +1,27 @@
+namespace Altalents.Report.Library
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using Telerik.Reporting;
+    using Telerik.Reporting.Drawing;
+
+	/// <summary>
+	/// Summary description for DossierCompetance.
+	/// </summary>
+    public partial class DossierCompetance : Telerik.Reporting.Report
+	{
+		public DossierCompetance()
+		{
+			//
+			// Required for telerik Reporting designer support
+			//
+			InitializeComponent();
+
+			//
+			// TODO: Add any constructor code after InitializeComponent call
+			//
+		}
+	}
+}
