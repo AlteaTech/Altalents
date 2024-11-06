0d4c31c712eca0ddad30a0c468db6c3cb39ed8f1
vincent romain <vincent.romain21@gmail.com>
2024-11-06 10:08:48 +0100

    autre test

--- ui/Altalents.API/Controllers/DossiersTechniquesController.cs
+++ ui/Altalents.API/Controllers/DossiersTechniquesController.cs
@@ -1,3 +1,4 @@
+using Altalents.Business.Services;
 using Altalents.Commun.Enums;
 using Altalents.IBusiness.DTO.Requesst;
 
@@ -8,10 +9,12 @@ namespace Altalents.API.Controllers
     public class DossiersTechniquesController
     {
         private readonly IDossierTechniqueService _dossierTechniqueService;
+        private readonly ICompetencesService _competencesService;
 
-        public DossiersTechniquesController(IDossierTechniqueService dossierTechniqueService)
+        public DossiersTechniquesController(IDossierTechniqueService dossierTechniqueService, ICompetencesService competencesService)
         {
             _dossierTechniqueService = dossierTechniqueService;
+            _competencesService = competencesService;
         }
 
         [HttpPost("", Name = "AddDossierTechnique")]
@@ -76,13 +79,13 @@ namespace Altalents.API.Controllers
             await _dossierTechniqueService.PutParlonsDeVousAsync(tokenRapide, request, cancellationToken);
         }
 
+        //WARNING FROM VROMAIN : Le formatage du nom de la route contient async alors que c'est pas le cas pour les auutres routes
         [HttpGet("{tokenAccesRapide}/nom-prenom", Name = "GetNomPrenomFromTokenAsync")]
         public async Task<NomPrenomPersonneDto> GetNomPrenomFromTokenAsync([FromRoute] Guid tokenAccesRapide, CancellationToken cancellationToken)
         {
             return await _dossierTechniqueService.GetNomPrenomFromTokenAsync(tokenAccesRapide, cancellationToken);
         }
-
-
+    
         [HttpPut("{tokenAccesRapide}/experiences", Name = "PutExperiences")]
         public async Task PutExperiencesAsync([FromRoute] Guid tokenAccesRapide, [FromBody] PutExperiencesRequestDto request, CancellationToken cancellationToken)
         {
@@ -100,10 +103,25 @@ namespace Altalents.API.Controllers
         {
             return await _dossierTechniqueService.GetDocumentsAsync(tokenAccesRapide, cancellationToken);
         }
+
         [HttpGet("{tokenAccesRapide}/generate-dt", Name = "GenerateDossierCompetenceFile")]
         public async Task<DocumentDto> GenerateDossierCompetenceFileAsync([FromRoute] Guid tokenAccesRapide, [FromQuery]TypeExportEnum? typeExportEnum, CancellationToken cancellationToken)
         {
             return await _dossierTechniqueService.GenerateDossierCompetenceFileAsync(tokenAccesRapide, typeExportEnum ?? TypeExportEnum.PDF, cancellationToken);
         }
+
+        [HttpGet("{tokenAccesRapide}/competences", Name = "GetCompetences")]
+        public async Task<List<CompetenceDto>> GetCompetencesAsync([FromRoute] Guid tokenAccesRapide, [FromQuery] string typeLiaisonCode, CancellationToken cancellationToken)
+        {
+            return await _competencesService.GetLiaisonCandidatByTypeAsync(tokenAccesRapide, typeLiaisonCode, cancellationToken);
+        }
+
+        [HttpPut("{tokenAccesRapide}/competences", Name = "PutNote")]
+        public async Task PutNote([FromRoute] Guid tokenAccesRapide, [FromBody] LiaisonExperienceUpdateNiveauDto request, CancellationToken cancellationToken)
+        {
+            await _competencesService.UpdateNiveauLiaisonAsync(request, cancellationToken);
+        }
+
+
     }
 }
