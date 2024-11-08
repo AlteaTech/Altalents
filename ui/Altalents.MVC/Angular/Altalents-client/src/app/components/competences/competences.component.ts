b5299224d70d58adbbcba89e049234088fa041d0
altea-si-tech <tech@altea-si.com>
2024-11-08 22:25:18 +0100

    retrait retour ligne en trop

--- ui/Altalents.MVC/Angular/Altalents-client/src/app/components/competences/competences.component.ts
+++ ui/Altalents.MVC/Angular/Altalents-client/src/app/components/competences/competences.component.ts
@@ -67,13 +67,13 @@ export class CompetencesComponent extends BaseComponent implements OnInit {
         // Vous pouvez également gérer les erreurs ici
       }
     });
-
-
   }
 
   public onEtoileCompetenceClick(competence: Competence, codeTypeLiaison : string, niveau: number): void {
+    
     competence.niveau = niveau; // Met à jour le niveau de la compétence
     this.isLoading = true;
+    
     // Appel API pour sauvegarder la note
     this.service.putNote(this.tokenDossierTechnique, this.populateRequestDto(competence, codeTypeLiaison)).subscribe({
       next: () => {
@@ -86,12 +86,6 @@ export class CompetencesComponent extends BaseComponent implements OnInit {
     });
   }
 
-  // public async onEtoileCOmpetenceClick(competence: Competence, codeTypeLiaison : string, numEToile : number): Promise<void> {
-  //   await firstValueFrom(this.service.putNote(this.tokenDossierTechnique,this.populateRequestDto(competence, codeTypeLiaison))).then(() => {
-  //     this.isLoading = false;
-  //   });
-  // }
-
   private populateRequestDto(competence: Competence, codeTypeLiaison : string): LiaisonExperienceUpdateNiveauDto {
 
     let dto = new LiaisonExperienceUpdateNiveauDto();
@@ -103,10 +97,4 @@ export class CompetencesComponent extends BaseComponent implements OnInit {
     return dto;
     
   }
-
-  // private loadData(): void {
-  //   // TODO : appeler le back pour avoir les infos
-  //   this.competences = "";
-  // }
-
 }
